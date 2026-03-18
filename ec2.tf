56fbec0942e28688aa43b4d5e909bdaf90ab0276
nayanmoudekar-web <nayanmoudekar@gmail.com>
2026-03-18 18:40:04 +0530

    new

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0f58b397bc5c1f2e8"   # Example Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
